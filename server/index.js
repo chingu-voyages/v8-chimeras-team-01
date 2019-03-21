67d2e6fd17441143230bb9be784f3ddf0e457b0e
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-03-20 22:30:11 -0500

    first commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
