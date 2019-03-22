8960895b1c6c096ebdd265cccb5573dce511249b
ZumDeWald <zumdewald@gmail.com>
2019-03-21 23:06:57 -0500

    Style and layout on Nav placeholder

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import './index.css';
 
 
 export default () => (
     <div id='nav'>
-      <header>Bragging Rights</header>
-      <h4 className="sub-header">Outwit your friends and earn <strong>Bragging Rights</strong></h4>
-
+      <header>
+        <h1 className="header">Bragging Rights</h1>
+        <h4 className="sub-header">Outwit your friends and earn <strong>Bragging Rights</strong></h4>
+      </header>
     </div>
 )
