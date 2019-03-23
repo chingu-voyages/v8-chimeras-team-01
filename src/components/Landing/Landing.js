999ca72de64a95cd752b5dddff2489f51325f270
ZumDeWald <zumdewald@gmail.com>
2019-03-22 23:22:12 -0500

    General style on Footer

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,7 +4,6 @@ import './index.css';
 export default function Landing() {
   return (
     <section id="landing">
-        <h1>Landing Component</h1>
         <article className="start-game-section">
           <button className="start-game-button">
             <p className="button-words"><strong>Are you a GameMaster??</strong></p>
