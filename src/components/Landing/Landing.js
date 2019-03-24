cfa4d27e83e9ef3b606ad1ac4011ac99f60997ce
ZumDeWald <zumdewald@gmail.com>
2019-03-23 21:44:12 -0500

    Set background in hth to fill on large

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,7 +6,7 @@ export default function Landing() {
 
   return (
     <section id="landing">
-        <article className="start-game-section">
+        <div className="start-game-section">
           <button className="start-game-button">
             <Link to="/games" className="link fbc">
               <p className="button-words"><strong>Are you a GameMaster??</strong></p>
@@ -19,25 +19,27 @@ export default function Landing() {
               <p className="button-words">Join A Game</p>
             </Link>
           </button>
-        </article>
+        </div>
 
-        <article className="how-to-host">
-          <h4 className="hth-header">Want to Host a Game?</h4>
-          <div className="how-to-box htb1">
-            <span className="htb-num">1</span>
-            <p>Choose "GameMaster" Option Above</p>
+        <div className="hth-container">
+          <div className="how-to-host">
+            <h4 className="hth-header">Want to Host a Game?</h4>
+            <div className="how-to-box htb1">
+              <span className="htb-num">1</span>
+              <p>Choose "GameMaster" Option Above</p>
+            </div>
+            <div className="how-to-box htb2">
+              <span className="htb-num">2</span>
+              <p>Select A Game</p>
+            </div>
+            <div className="how-to-box htb3">
+              <span className="htb-num">3</span>
+              <p>Invite Friends!</p>
+            </div>
           </div>
-          <div className="how-to-box htb2">
-            <span className="htb-num">2</span>
-            <p>Select A Game</p>
-          </div>
-          <div className="how-to-box htb3">
-            <span className="htb-num">3</span>
-            <p>Invite Friends!</p>
-          </div>
-        </article>
+        </div>
 
-        <article className="gameplay-example">
+        <div className="gameplay-example">
           <span className="ge-header">Gameplay</span>
           <span className="ge-description geb1">Setup a game with a few clicks</span>
           <span className="ge-pic geb2">SCREENSHOT</span>
@@ -45,7 +47,7 @@ export default function Landing() {
           <span className="ge-description geb4">Answer the questions as they come</span>
           <span className="ge-description geb5">See who won Bragging Rights</span>
           <span className="ge-pic geb6">SCREENSHOT</span>
-        </article>
+        </div>
     </section>
   )
 }
