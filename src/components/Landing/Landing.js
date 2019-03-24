393883a37ecedb6633dac0c2362709ef18fabc7a
ZumDeWald <zumdewald@gmail.com>
2019-03-24 14:30:40 -0500

    Set BG on ge and unsel view button

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -14,55 +14,57 @@ export default function Landing() {
           <i className="fas fa-gamepad fa-3x"></i>
         </div>
       </section>
-        <div className="start-game-section">
-          <button className="start-game-button">
-            <Link to="/games" className="link fbc">
-              <p className="button-words"><strong>Are you a GameMaster??</strong></p>
-              <p className="button-words">Host A Game</p>
-            </Link>
-          </button>
-          <button className="start-game-button">
-            <Link to="/join" className="link fbc">
-              <p className="button-words"><strong>Have a Code??</strong></p>
-              <p className="button-words">Join A Game</p>
-            </Link>
-          </button>
-        </div>
 
-        <div className="hth-container">
-          <div className="how-to-host">
-            <h4 className="hth-header fbc">Want to Host a Game?</h4>
-            <div className="how-to-box htb1">
-              <span className="htb-num">1</span>
-              <p>Choose "GameMaster" Option Above</p>
-            </div>
-            <div className="how-to-box htb2">
-              <span className="htb-num">2</span>
-              <p>Select A Game</p>
-            </div>
-            <div className="how-to-box htb3">
-              <span className="htb-num">3</span>
-              <p>Invite Friends!</p>
-            </div>
+      <div className="ge-container">
+        <div className="gameplay-example">
+          <div className="fbc">
+            <span className="ge-header fbc">How do I earn Bragging Rights?</span>
+          </div>
+          <div className="ge-box geb1">
+            <div className="ge-description fbc">Setup a game with a few clicks</div>
+          </div>
+          <div className="ge-box geb2">
+            <div className="ge-description fbc">Answer the questions as they come</div>
+          </div>
+          <div className="ge-box geb3">
+            <div className="ge-description fbc">See who won Bragging Rights</div>
           </div>
         </div>
+      </div>
 
-        <div className="ge-container">
-          <div className="gameplay-example">
-            <div className="fbc">
-              <span className="ge-header fbc">Gameplay</span>
-            </div>
-            <div className="ge-box geb1">
-              <div className="ge-description fbc">Setup a game with a few clicks</div>
-            </div>
-            <div className="ge-box geb2">
-              <div className="ge-description fbc">Answer the questions as they come</div>
-            </div>
-            <div className="ge-box geb3">
-              <div className="ge-description fbc">See who won Bragging Rights</div>
-            </div>
+      <div className="start-game-section">
+        <button className="start-game-button">
+          <Link to="/games" className="link fbc">
+            <p className="button-words"><strong>Are you a GameMaster??</strong></p>
+            <p className="button-words">Host A Game</p>
+          </Link>
+        </button>
+        <button className="start-game-button">
+          <Link to="/join" className="link fbc">
+            <p className="button-words"><strong>Have a Code??</strong></p>
+            <p className="button-words">Join A Game</p>
+          </Link>
+        </button>
+      </div>
+
+      <div className="hth-container">
+        <div className="how-to-host">
+          <h4 className="hth-header fbc">Want to Host a Game?</h4>
+          <div className="how-to-box htb1">
+            <span className="htb-num">1</span>
+            <p>Choose "GameMaster" Option Above</p>
+          </div>
+          <div className="how-to-box htb2">
+            <span className="htb-num">2</span>
+            <p>Select A Game</p>
+          </div>
+          <div className="how-to-box htb3">
+            <span className="htb-num">3</span>
+            <p>Invite Friends!</p>
           </div>
         </div>
+      </div>
+
     </section>
   )
 }
