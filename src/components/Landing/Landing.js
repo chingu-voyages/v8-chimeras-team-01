1c5f1e79ef6a76113e6d0fb5a4b3ec0db79e9fe3
ZumDeWald <zumdewald@gmail.com>
2019-03-24 15:58:54 -0500

    Sliding views working

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -16,6 +16,23 @@ export default function Landing() {
       buttons[0].classList.toggle("sel");
       buttons[1].classList.toggle("sel");
       setView(selection);
+      toggleView(selection);
+    };
+  }
+
+  /**
+   * @function [toggleView]
+   * @description [Add/Remove class to slide correct view into screen]
+   */
+  const toggleView = (selection) => {
+    let gameContainer = document.getElementsByClassName("start-game-container");
+    let geContainer = document.getElementsByClassName("ge-container");
+    if (selection === "info") {
+      gameContainer[0].classList.add("shift-right");
+      geContainer[0].classList.remove("shift-left");
+    } else if (selection === "game") {
+      gameContainer[0].classList.remove("shift-right");
+      geContainer[0].classList.add("shift-left");
     }
   }
 
@@ -32,56 +49,62 @@ export default function Landing() {
         </div>
       </section>
 
-      <div className="ge-container">
-        <div className="gameplay-example">
-          <div className="fbc">
-            <h4 className="ge-header fbc">How do I earn Bragging Rights?</h4>
-          </div>
-          <div className="ge-box geb1">
-            <div className="ge-description fbc">Setup a game with a few clicks</div>
-          </div>
-          <div className="ge-box geb2">
-            <div className="ge-description fbc">Answer the questions as they come</div>
-          </div>
-          <div className="ge-box geb3">
-            <div className="ge-description fbc">See who won Bragging Rights</div>
+      <section className="view-container">
+
+        <div className="ge-container">
+          <div className="gameplay-example">
+            <div className="fbc">
+              <h4 className="ge-header fbc">How do I earn Bragging Rights?</h4>
+            </div>
+            <div className="ge-box geb1">
+              <div className="ge-description fbc">Setup a game with a few clicks</div>
+            </div>
+            <div className="ge-box geb2">
+              <div className="ge-description fbc">Answer the questions as they come</div>
+            </div>
+            <div className="ge-box geb3">
+              <div className="ge-description fbc">See who won Bragging Rights</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="start-game-section">
-        <h4 className="fbc">Quick Launch</h4>
-        <button className="start-game-button fbc">
-          <Link to="/games" className="link fbc">
-            <p className="button-words"><strong>Are you a GameMaster??</strong></p>
-            <p className="button-words">Host A Game</p>
-          </Link>
-        </button>
-        <button className="start-game-button fbc">
-          <Link to="/join" className="link fbc">
-            <p className="button-words"><strong>Have a Code??</strong></p>
-            <p className="button-words">Join A Game</p>
-          </Link>
-        </button>
-      </div>
+        <section className="start-game-container shift-right">
+          <div className="start-game-section">
+            <h4 className="fbc">Quick Launch</h4>
+            <button className="start-game-button fbc">
+              <Link to="/games" className="link fbc">
+                <p className="button-words"><strong>Are you a GameMaster??</strong></p>
+                <p className="button-words">Host A Game</p>
+              </Link>
+            </button>
+            <button className="start-game-button fbc">
+              <Link to="/join" className="link fbc">
+                <p className="button-words"><strong>Have a Code??</strong></p>
+                <p className="button-words">Join A Game</p>
+              </Link>
+            </button>
+          </div>
 
-      <div className="hth-container">
-        <div className="how-to-host">
-          <h4 className="hth-header fbc">Want to Host a Game?</h4>
-          <div className="how-to-box htb1">
-            <span className="htb-num">1</span>
-            <p>Choose "GameMaster" Option Above</p>
+          <div className="hth-container">
+            <div className="how-to-host">
+              <h4 className="hth-header fbc">Want to Host a Game?</h4>
+              <div className="how-to-box htb1">
+                <span className="htb-num">1</span>
+                <p>Choose "GameMaster" Option Above</p>
+              </div>
+              <div className="how-to-box htb2">
+                <span className="htb-num">2</span>
+                <p>Select A Game</p>
+              </div>
+              <div className="how-to-box htb3">
+                <span className="htb-num">3</span>
+                <p>Invite Friends!</p>
+              </div>
+            </div>
           </div>
-          <div className="how-to-box htb2">
-            <span className="htb-num">2</span>
-            <p>Select A Game</p>
-          </div>
-          <div className="how-to-box htb3">
-            <span className="htb-num">3</span>
-            <p>Invite Friends!</p>
-          </div>
-        </div>
-      </div>
+        </section>
+
+      </section>
 
     </section>
   )
