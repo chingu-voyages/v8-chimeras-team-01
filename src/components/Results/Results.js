1c2ba4c27cbe0f3669b4bfac49cea3dac66a33c8
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-03-24 16:30:00 -0700

    2.8 Results Component - Initial setup

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 export default function Results() {
   return (
     <div id="results">
-      <h1>Results Component</h1>
+      <div className="res__header">
+        <h1 className="res__title">
+          Nice work ${'{winnerName}'}! Time to BRAG
+        </h1>
+      </div>
+      <div className="res__container">
+        <div className="res__wrapper">
+          <div className="res__bar">
+            <span>${`{secondPlace}`}</span>
+            <div className="res__bar-inner">
+            </div>
+          </div>
+          <div className="res__bar">
+            <span>${`{winner}`}</span>
+            <div className="res__bar-inner">
+            </div>
+          </div>
+          <div className="res__bar">
+            <span>${`{thirdPlace}`}</span>
+            <div className="res__bar-inner">
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className="res__btn">
+        <Link 
+          to="/start-game" className="res__btn-anchor"
+        >
+          <p className="">Time for a REMATCH??</p>
+        </Link>
+      </button>
     </div>
   )
 }
